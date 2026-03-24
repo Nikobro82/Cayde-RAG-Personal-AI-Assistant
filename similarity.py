37a3e5b35ebae76968b7cc5157d50def35487dae
Nikobro82 <jeffreyniko19@gmail.com>
2026-03-23 22:42:36 -0400

    FIXED AGAIN

--- similarity.py
+++ similarity.py
@@ -1,28 +1,5 @@
-import yfinance as yf
-import pandas as pd
 import torch 
-import torch.nn.functional as F
-from torchviz import make_dot
-import matplotlib.pyplot as plt
-import torch.optim as optim
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-from sklearn.preprocessing import StandardScaler
-from torchvision import datasets, transforms
-from torch.utils.data import DataLoader
 
-import os
-import cv2
-
-import gradio as gr
-
-import lightning as L
-
-from transformers import AutoTokenizer
-
-import time
-
-import streamlit as st
 
 
 def get_scores(v1, v2, k):
